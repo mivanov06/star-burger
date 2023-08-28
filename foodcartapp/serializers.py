09e264514bf9e3d770c375a253c83b5261a1434e
mivanov06 <mivanov06@yandex.ru>
2023-08-28 23:11:18 +1000

    edited OrderSerializer

--- foodcartapp/serializers.py
+++ foodcartapp/serializers.py
@@ -17,43 +17,22 @@ class OrderedItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProductOrderItem
-        fields = ('product', 'quantity')
+        fields = ('id', 'product', 'quantity')
 
 
 class OrderSerializer(serializers.ModelSerializer):
     products = OrderedItemSerializer(many=True, required=True, source="ordered_items", allow_empty=False)
-    status_display = serializers.CharField(
-        source='get_status_display'
-    )
-    payment_display = serializers.CharField(
-        source='get_payment_display'
-    )
-    firstname = serializers.CharField(
-        required=True
-    )
-    lastname = serializers.CharField(
-        required=True
-    )
-    phonenumber = serializers.CharField(
-        required=True
-    )
-    address = serializers.CharField(
-        required=True)
-    total_amount = serializers.FloatField(
-        read_only=True
-    )
-    comment = serializers.CharField(
-    )
 
     class Meta:
         model = Order
-        fields = ("products", "status_display", "firstname", "lastname", "phonenumber", "address",
-                  "comment", "id", "total_amount", "payment_display")
+        fields = ("products", "firstname", "lastname", "phonenumber", "address",
+                  "comment", "id")
         read_only_fields = ("id", *fields)
 
     @atomic
     def create(self, validated_data):
         ordered_items_data = validated_data.pop("ordered_items")
+        print(ordered_items_data)
         order = Order.objects.create(**validated_data)
         for entity in ordered_items_data:
             ProductOrderItem.objects.create(**entity, order=order)
