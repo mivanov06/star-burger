31ec32a36221ddab6cc22c980193851de9761173
mivanov06 <mivanov06@yandex.ru>
2023-08-28 23:12:25 +1000

    edited default DEBUG

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -14,7 +14,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 YANDEX_GEO_KEY = env('YANDEX_GEO_KEY')
 
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env.bool('DEBUG', True)
+DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
