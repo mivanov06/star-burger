b11ba98a6b1523a092195b13383f2f75e1c2642b
root <mivanov06@yandex.ru>
2023-09-24 02:12:36 +0000

    edited deploy

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -3,11 +3,11 @@
 set -e
 
 git -C /opt/star-burger pull
-/opt/star-burger/myvenv/bin/pip3 install -r /opt/star-burger/requirements.txt
-/opt/star-burger/myvenv/bin/python3.10 /opt/star-burger/manage.py makemigrations --dry-run --check
+/opt/star-burger/venv/bin/pip install -r /opt/star-burger/requirements.txt
+/opt/star-burger/venv/bin/python3.10 /opt/star-burger/manage.py makemigrations --dry-run --check
 npm ci --dev --prefix /opt/star-burger
-/opt/star-burger/myvenv/bin/python3.10 /opt/star-burger/manage.py collectstatic --noinput
-/opt/star-burger/myvenv/bin/python3.10 /opt/star-burger/manage.py migrate --noinput
+/opt/star-burger/venv/bin/python3.10 /opt/star-burger/manage.py collectstatic --noinput
+/opt/star-burger/venv/bin/python3.10 /opt/star-burger/manage.py migrate --noinput
 systemctl restart star-burger.target
 systemctl reload nginx
 http POST https://api.rollbar.com/api/1/deploy X-Rollbar-Access-Token:$ROLLBAR_TOKEN environment=$ROLLBAR_ENV revision=$(git rev-parse --verify HEAD) rollbar_username=pcorpx comment="new deploy" status=succeeded
