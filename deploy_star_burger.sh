abfde6f521ebefd2eca011a5a63298b665672666
mivanov06 <mivanov06@yandex.ru>
2023-10-07 14:43:35 +1000

    edited deploy_star_burger.sh

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -2,14 +2,39 @@
 
 set -e
 
-git -C /opt/star-burger pull
-/opt/star-burger/venv/bin/pip install -r /opt/star-burger/requirements.txt
-/opt/star-burger/venv/bin/python3.10 /opt/star-burger/manage.py makemigrations --dry-run --check
+cd /opt/star-burger/
+
+git pull
+source venv/bin/activate
+
+pip install -r requirements.txt
+
 npm ci --dev --prefix /opt/star-burger
-/opt/star-burger/venv/bin/python3.10 /opt/star-burger/manage.py collectstatic --noinput
-/opt/star-burger/venv/bin/python3.10 /opt/star-burger/manage.py migrate --noinput
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
 systemctl restart star-burger.target
 systemctl reload nginx
-http POST https://api.rollbar.com/api/1/deploy X-Rollbar-Access-Token:$ROLLBAR_TOKEN environment=$ROLLBAR_ENV revision=$(git rev-parse --verify HEAD) rollbar_username=mivanov06 comment="new deploy" status=succeeded
-echo "Deploy has successefully finished"
+echo "reload nginx"
 
+rollbar_token=$ROLLBAR_TOKEN
+rollbar_env=$ROLLBAR_ENV
+commit_hash=$(git rev-parse --verify HEAD)
+
+curl --request POST --url https://api.rollbar.com/api/1/deploy \
+     --header 'X-Rollbar-Access-Token: $ROLLBAR_TOKEN' \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data '
+            {
+              "environment": "$ROLLBAR_ENV",
+              "revision": "$commit_hash",
+              "rollbar_username": "mivanov06",
+              "local_username": "mivanov06",
+              "comment": "new deploy",
+              "status": "succeeded"
+            }
+            '
+
+echo "Deploy has successefully finished"
