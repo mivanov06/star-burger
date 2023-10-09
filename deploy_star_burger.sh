64dca35d861441f294936a288b3df36b56b8f4f9
root <mivanov06@yandex.ru>
2023-10-09 10:37:40 +0000

    edited deploy_star_burgrer

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -9,7 +9,7 @@ source venv/bin/activate
 
 pip install -r requirements.txt
 
-#npm ci
+npm ci
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
